77e621eb13f034f35ae00037083d7ea6c1dc37f5
Gabriel Bravo <gabrielbm19@gmail.com>
2020-03-31 11:42:54 -0300

    Revert "commit"
    This reverts commit ac8cf16fcdbb6dc240f3f7d5e4e04089afb00336.

--- server/config/config.js
+++
@@ -1,4 +0,0 @@
-process.env.PORT = process.env.PORT || 3000;
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-process.env.TOKEN_EXPIRES = '24h' 
-process.env.SEED = process.env.SEED || 'secret-desarrollo';
--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use( require ('./login' ));
-app.use( require ('./verify'));
-
-module.exports = app;
